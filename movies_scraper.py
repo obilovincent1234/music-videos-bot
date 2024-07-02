cd3010ba91cbf1c1fcd7d1730e87eda38fe51907
obilovincent1234 <110498090+obilovincent1234@users.noreply.github.com>
2024-07-02 22:49:10 +0100

    Update movies_scraper.py

--- movies_scraper.py
+++ movies_scraper.py
@@ -1,41 +1,39 @@
+# movies_scraper.py
+
 import requests
 from bs4 import BeautifulSoup
 
-
-url_list = {}
-api_key = "e59cb11de157619aee585ddb38129d860ea455a6"
-
-
+# Function to search for movies
 def search_movies(query):
     movies_list = []
-    movies_details = {}
-    website = BeautifulSoup(requests.get(f"https://185.53.88.104/?s={query.replace(' ', '+')}").text, "html.parser")
-    movies = website.find_all("a", {'class': 'ml-mask jt'})
-    for movie in movies:
-        if movie:
-            movies_details["id"] = f"link{movies.index(movie)}"
-            movies_details["title"] = movie.find("span", {'class': 'mli-info'}).text
-            url_list[movies_details["id"]] = movie['href']
-        movies_list.append(movies_details)
-        movies_details = {}
+    url = f"https://musicstudio.com.ng/search?q={query}"
+    response = requests.get(url)
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.content, 'html.parser')
+        results = soup.find_all('div', class_='movie-item')
+
+        for result in results:
+            title = result.find('h2', class_='title').text.strip()
+            link = result.find('a')['href']
+            movies_list.append({"title": title, "link": link})
+
     return movies_list
 
-
-def get_movie(query):
+# Function to get movie details
+def get_movie(movie_id):
     movie_details = {}
-    movie_page_link = BeautifulSoup(requests.get(f"{url_list[query]}").text, "html.parser")
-    if movie_page_link:
-        title = movie_page_link.find("div", {'class': 'mvic-desc'}).h3.text
-        movie_details["title"] = title
-        img = movie_page_link.find("div", {'class': 'mvic-thumb'})['data-bg']
-        movie_details["img"] = img
-        links = movie_page_link.find_all("a", {'rel': 'noopener', 'data-wpel-link': 'internal'})
-        final_links = {}
-        for i in links:
-            url = f"https://urlshortx.com/api?api={api_key}&url={i['href']}"
-            response = requests.get(url)
-            link = response.json()
-            final_links[f"{i.text}"] = link['shortenedUrl']
-        movie_details["links"] = final_links
-    return movie_details
+    url = f"https://musicstudio.com.ng/movie/{movie_id}"
+    response = requests.get(url)
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.content, 'html.parser')
+        title = soup.find('h1', class_='movie-title').text.strip()
+        img_url = soup.find('img', class_='movie-img')['src']
+        download_links = soup.find('div', class_='download-links').find_all('a')
 
+        links = {}
+        for link in download_links:
+            links[link.text.strip()] = link['href']
+
+        movie_details = {"title": title, "img": img_url, "links": links}
+
+    return movie_details
