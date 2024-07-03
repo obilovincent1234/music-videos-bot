3537fdcd1ffb51c2e99d19dd8a3d7080d5f12db8
obilovincent1234 <110498090+obilovincent1234@users.noreply.github.com>
2024-07-03 18:09:03 +0100

    Delete secret.py

--- secret.py
+++
@@ -1,6 +0,0 @@
-from typing import Final
-
-
-TOKEN: Final = '7408716433:AAF-AdkD0CJkVCtccdmfGwCq9wzd7BrQlTQ'
-BOT_USERNAME: Final = '@Music_video1_bot'
-TMDB_API_KEY = '25f9f07c72a6a05dd25ff07138e2feaa'
