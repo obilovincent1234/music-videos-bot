be5daef3e93f3bd14ec39cab20dcccbbc916caa3
obilovincent1234 <110498090+obilovincent1234@users.noreply.github.com>
2024-07-03 01:12:49 +0100

    Update index.py

--- index.py
+++ index.py
@@ -1,3 +1,5 @@
+# index.py
+
 import os
 from io import BytesIO
 from queue import Queue
@@ -7,17 +9,14 @@ from telegram import Bot, Update, InlineKeyboardButton, InlineKeyboardMarkup
 from telegram.ext import CommandHandler, MessageHandler, Filters, CallbackQueryHandler, Dispatcher
 from movies_scraper import search_movies, get_movie
 
-
 TOKEN = os.getenv("TOKEN")
-URL = "https://movie-bot-vert.vercel.app"
+URL = "https://music-videos-bot.vercel.app/"  # Ensure this is your correct URL
 bot = Bot(TOKEN)
-
 
 def welcome(update, context) -> None:
     update.message.reply_text(f"Hello {update.message.from_user.first_name}, Welcome to AI Movies.\n"
                               f"🔥 Download Your Favourite Movies For 💯 Free And 🍿 Enjoy it.")
     update.message.reply_text("👇 Enter Movie Name 👇")
-
 
 def find_movie(update, context):
     search_results = update.message.reply_text("Processing...")
@@ -32,7 +31,6 @@ def find_movie(update, context):
         search_results.edit_text('Search Results...', reply_markup=reply_markup)
     else:
         search_results.edit_text('Sorry 🙏, No Result Found!\nCheck If You Have Misspelled The Movie Name.')
-
 
 def movie_result(update, context) -> None:
     query = update.callback_query
@@ -51,7 +49,6 @@ def movie_result(update, context) -> None:
     else:
         query.message.reply_text(text=caption)
 
-
 def setup():
     update_queue = Queue()
     dispatcher = Dispatcher(bot, update_queue, use_context=True)
@@ -60,21 +57,17 @@ def setup():
     dispatcher.add_handler(CallbackQueryHandler(movie_result))
     return dispatcher
 
-
 app = Flask(__name__)
-
 
 @app.route('/')
 def index():
     return 'Hello World!'
-
 
 @app.route('/{}'.format(TOKEN), methods=['GET', 'POST'])
 def respond():
     update = Update.de_json(request.get_json(force=True), bot)
     setup().process_update(update)
     return 'ok'
-
 
 @app.route('/setwebhook', methods=['GET', 'POST'])
 def set_webhook():
