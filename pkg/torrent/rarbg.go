1ea59e75c924f4d5edac2728065fb83fa39cac40
obilovincent1234 <obilochigozie5@gmail.com>
2024-08-23 02:14:43 +0100

    Update rarbg.go

--- pkg/torrent/rarbg.go
+++ pkg/torrent/rarbg.go
@@ -1,51 +1,35 @@
 package torrent
 
 import (
+	"encoding/json"
 	"errors"
-	"log"
+	"fmt"
+	"net/http"
 	"strings"
 	"time"
-
-	rarbg "github.com/qopher/go-torrentapi"
 )
 
 const (
-	app    = "moviemagnetbot" // app name is required
-	ranked = true             // Should results be ranked
-	sort   = "seeders"        // Sort order (seeders, leechers, last)
-	format = "json_extended"  // Format (json, json_extended)
-	limit  = 25               // Limit of results (25, 50, 100)
+	apiBaseURL = "https://1337x.to/api" // Base URL for 1337x API
+	sort       = "seeders"              // Sort order (seeders, leechers, last)
+	limit       = 25                    // Limit of results
 )
 
 var (
-	rapi *rarbg.API
-
-	errRARBG      = errors.New("We encountered an error while finding magnet links, please try again")
+	err1337x      = errors.New("We encountered an error while finding magnet links, please try again")
 	errNoTorrents = errors.New("We have no magnet links for this movie now, please come back later")
 )
 
-// InitRARBG init RARBG API
-func InitRARBG() {
-	api, err := rarbg.Init(app)
-	if err != nil {
-		log.Fatalf("error while querying rarbg: %s", err)
-	}
-	rapi = api
-}
-
-// Search take keyword to search torrents
+// Search takes keyword to search torrents
 func Search(keyword string, limit int) (*[]Torrent, error) {
-
 	keywords := strings.Split(keyword, " ")
 
-	torrentResults, err := searchByServiceID(keywords[0], keywords[1])
+	torrentResults, err := searchByKeyword(keywords[0], keywords[1])
 	if err != nil {
-		log.Printf("error while querying rarbg: %s", err)
-		return nil, errRARBG
+		return nil, err1337x
 	}
 
 	if len(torrentResults) == 0 {
-		log.Printf("no torrents found for this movie: %s", keywords[1])
 		return nil, errNoTorrents
 	}
 
@@ -57,43 +41,50 @@ func Search(keyword string, limit int) (*[]Torrent, error) {
 	return torrents, nil
 }
 
-func searchByServiceID(service string, id string) (rarbg.TorrentResults, error) {
-	switch service {
-	case "imdb":
-		rapi.SearchIMDb(id)
-	case "tmdb":
-		rapi.SearchTheMovieDb(id)
+func searchByKeyword(service string, id string) ([]TorrentResult, error) {
+	url := fmt.Sprintf("%s/search/%s/%s", apiBaseURL, service, id)
+	resp, err := http.Get(url)
+	if err != nil {
+		return nil, err
 	}
-	rapi.Ranked(ranked).Sort(sort).Format(format).Limit(limit)
-	return rapi.Search()
+	defer resp.Body.Close()
+
+	if resp.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("error: received status code %d", resp.StatusCode)
+	}
+
+	var results struct {
+		Torrents []TorrentResult `json:"results"`
+	}
+	if err := json.NewDecoder(resp.Body).Decode(&results); err != nil {
+		return nil, err
+	}
+
+	return results.Torrents, nil
 }
 
-func newTorrentsBySearch(trs *rarbg.TorrentResults, limit int) (*[]Torrent, error) {
-
+func newTorrentsBySearch(trs *[]TorrentResult, limit int) (*[]Torrent, error) {
 	torrents := []Torrent{}
-
 	for i, tr := range *trs {
 		if i == limit {
 			break
 		}
-		t, err := saveTorrentFromRARBG(tr)
+		t, err := saveTorrentFrom1337x(tr)
 		if err != nil {
 			continue
 		}
 		torrents = append(torrents, *t)
 	}
-
 	return &torrents, nil
 }
 
-func saveTorrentFromRARBG(tr rarbg.TorrentResult) (*Torrent, error) {
-
+func saveTorrentFrom1337x(tr TorrentResult) (*Torrent, error) {
 	t := &Torrent{TorrentResult: tr}
 	if tr.Title == "" {
 		return t, errors.New("Torrent title should not be empty")
 	}
 
-	// use `PubDate` as an unique command for each torrent
+	// Assuming `PubDate` is in a valid format or remove if not applicable
 	pubDate, err := time.Parse("2006-01-02 15:04:05 +0000", t.PubDate)
 	if err != nil {
 		return t, err
