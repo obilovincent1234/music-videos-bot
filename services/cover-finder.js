84b2fd12459279241fcc0a7175d42abab24cab5b
obilovincent1234 <obilochigozie5@gmail.com>
2024-08-28 23:29:12 +0100

    Update cover-finder.js

--- services/cover-finder.js
+++ services/cover-finder.js
@@ -11,7 +11,7 @@ require('dotenv').config({
     path: './../.env'
 })
 
-mongoose.connect(`mongodb+srv://musicvideo:Vincentdepaul123@cluster0.cen1xf9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`, {
+mongoose.connect("mongodb+srv://musicvideo:Vincentdepaul123@cluster0.cen1xf9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0", {
     useNewUrlParser: true,
     authSource: 'admin',
     useUnifiedTopology: true
