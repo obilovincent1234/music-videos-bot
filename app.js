95fc8d527e6fd0cd0dab64342f598ef7322f1b01
obilovincent1234 <obilochigozie5@gmail.com>
2024-08-28 23:56:48 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,77 +1,65 @@
-var mongoose = require("mongoose");
-var express = require("express");
-var cors = require('cors')
+const mongoose = require('mongoose');
+const express = require('express');
+const cors = require('cors');
+const path = require('path');
+const cron = require('node-cron');
+const cronService = require('./services/cron');
+require('dotenv').config();
 
-//.env config
-require('dotenv').config()
+// Retrieve MongoDB URI from environment variables
+const mongoURI = process.env.MONGO_URI;
 
-const mongoURI = `mongodb+srv://musicvideo:Vincentdepaul123@cluster0.cen1xf9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+const messagesRouter = require('./routes/messages');
+const searchesRouter = require('./routes/searches');
+const moviesRouter = require('./routes/movies');
 
-var indexRouter = require("./routes/index");
-var usersRouter = require("./routes/users");
-var messagesRouter = require('./routes/messages');
+const app = express();
 
-var app = express();
-
+// Middleware setup
 app.use(cors());
-// app.use(logger("dev"));
 app.use(express.json());
-app.use(express.urlencoded({
-    extended: true
-}));
+app.use(express.urlencoded({ extended: true }));
 
+// Routes
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+app.use('/messages', messagesRouter);
+app.use('/searches', searchesRouter);
+app.use('/movies', moviesRouter);
 
-let searchesRouter = require('./routes/searches');
-let moviesRouter = require('./routes/movies');
-
-app.use("/", indexRouter);
-app.use("/users", usersRouter);
-app.use("/messages", messagesRouter);
-app.use("/searches", searchesRouter);
-app.use("/movies", moviesRouter);
-
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-    res.status(404);
-    res.json({
-        ok: false
-    })
+// Catch 404 errors
+app.use((req, res, next) => {
+    res.status(404).json({ ok: false });
 });
-// error handler
-app.use(function (err, req, res, next) {
-    // set locals, only providing error in development
+
+// Error handler
+app.use((err, req, res, next) => {
     res.locals.message = err.message;
-    res.locals.error = process.env.DEBUG === true ? err : {};
-
-    // render the error page
-    res.status(err.status || 500);
-    res.json({
-        stauts: 500,
-        error: err.message
-    });
+    res.locals.error = process.env.DEBUG === 'true' ? err : {};
+    res.status(err.status || 500).json({ status: err.status || 500, error: err.message });
 });
+
+// MongoDB connection
 mongoose.connect(mongoURI, {
     useNewUrlParser: true,
-    authSource: 'admin',
-    useUnifiedTopology: true
-});
-var db = mongoose.connection;
-db.on("error", console.error.bind(console, "connection error:"));
-db.once("open", function callback() {
-    console.log("connected to com_watch database");
-    app.listen(
-        3000,
-        console.log("server is running: ", new Date().toTimeString())
-    );
+    useUnifiedTopology: true,
+    authSource: 'admin'
 });
 
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'Connection error:'));
+db.once('open', () => {
+    console.log('Connected to com_watch database');
+    app.listen(3000, () => {
+        console.log('Server is running on port 3000:', new Date().toTimeString());
+    });
+});
 
-
-const cron = require("node-cron");
-const cronService = require("./services/cron")
-cron.schedule("0 */15 * * * *", function () {
-// cron.schedule("* * * * * *", function () {
-    console.log('getNoResultSuggestion every 15 minutes');
+// Cron job
+cron.schedule('0 */15 * * * *', () => {
+    console.log('Running getNoResultSuggestion every 15 minutes');
     cronService.getNoResultSuggestion();
 });
 
