c3ccb632f87d3f0c5e9eb735ecedb5d61b75f04f
obilovincent1234 <obilochigozie5@gmail.com>
2024-08-28 23:56:43 +0100

    Update cover-finder.js

--- services/cover-finder.js
+++ services/cover-finder.js
@@ -1,59 +1,60 @@
-var Search = require('./../models/Search')
-var Movie = require('./../models/Movie')
-var Log = require('./../models/Log')
-const moment = require('moment');
-const rp = require('request-promise')
-const _ = require('lodash');
+const mongoose = require('mongoose');
+const axios = require('axios'); // Replace request with axios
 const cheerio = require('cheerio');
-const mongoose = require('mongoose')
-const request = require('request')
-require('dotenv').config({
-    path: './../.env'
-})
+require('dotenv').config({ path: './../.env' });
 
-const mongoURI = `mongodb+srv://musicvideo:Vincentdepaul123@cluster0.cen1xf9.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+// Models
+const Search = require('./../models/Search');
+const Movie = require('./../models/Movie');
+const Log = require('./../models/Log');
 
-
-mongoose.connect(mongoURI, {
+// Database connection
+mongoose.connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
-    authSource: 'admin',
-    useUnifiedTopology: true
+    useUnifiedTopology: true,
+    authSource: 'admin'
 });
-var db = mongoose.connection;
 
+const db = mongoose.connection;
 
-
-var findCovers = async function () {
-
-
-    let movies = await Movie.find({
-        cover: null
-    }).limit(1);
-
-    console.log('s')
-    console.log(`movies in db => ${movies.length}`)
-    movies.forEach(async (movie) => {
-        let movieName = movie.name.replace(/((20|19)[0-9]{2})/g,'');
-        let url = encodeURI(`https://www.imdb.com/search/title/?title=${movieName}&title_type=feature,tv_movie,tv_series,short&adult=include&view=simple`);
-        request(url, function (error, response, body) {
-
-
-            let $ = cheerio.load(body);
-            if ($('.mode-simple').length === 0) {
-                console.log(`no result: @${movieName}@`);
-                return false;
-            }
-            let src = $('.mode-simple').eq(0).find('.lister-item-image').find('img').attr('src');
-            console.log(`founded ${movieName} => ${src}`)
-
-        })
-
-    })
-
-}
-
-db.on("error", console.error.bind(console, "connection error:"));
-db.once("open", function callback() {
-    console.log('db connected')
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', function () {
+    console.log('Database connected');
     findCovers();
 });
+
+// Find covers function
+const findCovers = async function () {
+    try {
+        let movies = await Movie.find({ cover: null }).limit(10); // Increase limit if needed
+
+        console.log(`Movies in DB => ${movies.length}`);
+
+        for (let movie of movies) {
+            let movieName = movie.name.replace(/((20|19)[0-9]{2})/g, '');
+            let url = encodeURI(`https://www.imdb.com/search/title/?title=${movieName}&title_type=feature,tv_movie,tv_series,short&adult=include&view=simple`);
+            
+            try {
+                const { data } = await axios.get(url);
+                const $ = cheerio.load(data);
+                
+                if ($('.mode-simple').length === 0) {
+                    console.log(`No result for: @${movieName}@`);
+                    continue;
+                }
+                
+                let src = $('.mode-simple').eq(0).find('.lister-item-image').find('img').attr('src');
+                console.log(`Found ${movieName} => ${src}`);
+
+                // You can update the movie with the cover here if needed
+                await Movie.updateOne({ _id: movie._id }, { cover: src });
+                
+            } catch (error) {
+                console.error(`Error fetching cover for: ${movieName}`, error);
+            }
+        }
+
+    } catch (error) {
+        console.error('Error finding covers:', error);
+    }
+};
